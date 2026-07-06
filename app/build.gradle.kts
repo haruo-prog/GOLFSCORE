a99ce6df7cdb090108fab67cc35f53c04d636683
haruo-prog <haruo@nk-ts.jp>
2026-07-06 15:04:16 +0900

    Add Android app Gradle module

---
+++ app/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "jp.co.nkts.scoremanager"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "jp.co.nkts.scoremanager"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 12
+        versionName = "1.2.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
