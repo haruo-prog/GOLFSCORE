0059377983ba3e0f6c8b2725b822c7f72b24cafb
haruo-prog <haruo@nk-ts.jp>
2026-07-06 22:07:45 +0900

    Set app version 1.8.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "jp.co.nkts.scoremanager"
         minSdk = 26
         targetSdk = 36
-        versionCode = 18
-        versionName = "1.7.0"
+        versionCode = 19
+        versionName = "1.8.0"
     }
 
     compileOptions {
