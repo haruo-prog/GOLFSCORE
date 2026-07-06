539f814c3f57d2845fb8dbc2512ba1a5be404156
haruo-prog <haruo@nk-ts.jp>
2026-07-06 16:24:46 +0900

    Bump version to 1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "jp.co.nkts.scoremanager"
         minSdk = 26
         targetSdk = 36
-        versionCode = 13
-        versionName = "1.3.0"
+        versionCode = 14
+        versionName = "1.4.0"
     }
 
     compileOptions {
