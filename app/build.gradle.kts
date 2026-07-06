90ce4788815858bb03231a7a512dc80bf97a45b3
haruo-prog <haruo@nk-ts.jp>
2026-07-07 07:51:27 +0900

    Set version 1.14.0 for V2.6 language expansion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "jp.co.nkts.scoremanager"
         minSdk = 26
         targetSdk = 36
-        versionCode = 25
-        versionName = "1.13.0"
+        versionCode = 26
+        versionName = "1.14.0"
     }
 
     compileOptions {
