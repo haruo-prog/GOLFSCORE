c1d1ff5c86296c638cdac1a2bb0a1659727e53fe
haruo-prog <haruo@nk-ts.jp>
2026-07-23 08:04:23 +0900

    Set version 1.19.0 for full date overhaul V2.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "jp.co.nkts.scoremanager"
         minSdk = 26
         targetSdk = 36
-        versionCode = 30
-        versionName = "1.18.0"
+        versionCode = 31
+        versionName = "1.19.0"
     }
 
     flavorDimensions += "edition"
