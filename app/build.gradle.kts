d057313b8cc379f4dccfaafb3fba4415af2fe8f5
haruo-prog <haruo@nk-ts.jp>
2026-07-25 17:43:10 +0900

    Set V2.14 version for player profile scorecard build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "jp.co.nkts.scoremanager"
         minSdk = 26
         targetSdk = 36
-        versionCode = 33
-        versionName = "1.21.0"
+        versionCode = 34
+        versionName = "1.22.0"
     }
 
     flavorDimensions += "edition"
